4dceb8044124a0ba9011b47751e36af0c0cd9bc5
Licesur <Sovakod@gmail.com>
2024-09-11 10:56:18 +0300

    file with db data added

---
+++ src/main/java/ru/sova/educationapp/EducationalSystemApp/EducationalSystemAppApplication.java
@@ -0,0 +1,13 @@
+package ru.sova.educationapp.EducationalSystemApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationalSystemAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducationalSystemAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/sova/educationapp/EducationalSystemApp/EducationalSystemAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sova.educationapp.EducationalSystemApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationalSystemAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
